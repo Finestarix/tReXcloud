57c1457b4518299cd3174ab709aced91245b3015
Renaldy <irenaldyleonarto@gmail.com>
2022-06-18 12:00:37 +0700

    feat: initial project

---
+++ components/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title><?= $title ?> | ReXcloud</title>
+    <link rel="shortcut icon" type="image/x-icon" href="public/favicon.ico">
+
+    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+    <script src="https://unpkg.com/alpinejs@3.10.2/dist/cdn.min.js"></script>
+</head>
