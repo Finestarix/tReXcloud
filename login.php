28b3ffb93fdbbb09a0bae837933b778667e666a6
Renaldy <irenaldyleonarto@gmail.com>
2022-07-14 10:12:22 +0700

    fix: change login username

--- login.php
+++ login.php
@@ -31,12 +31,15 @@ if (isset($_SESSION["USER"])) {
                     <form action="controllers/loginController.php" method="POST" class="space-y-3">
 
                         <div class="space-y-1">
-                            <label for="username" class="block text-sm font-medium text-gray-700">
-                                Email Address / Username
+                            <label for="username" class="text-sm font-medium text-gray-700">
+                                Username
                             </label>
-                            <div class="mt-1">
+                            <div class="mt-1 flex rounded-md shadow-sm">
                                 <input id="username" name="username" type="text"
-                                       class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm">
+                                       class="appearance-none px-3 py-2 w-full border border-gray-300 rounded-l-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm">
+                                <span class="inline-flex items-center px-3 rounded-r-md border border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
+                                @trexcloud.com
+                            </span>
                             </div>
                         </div>
 
