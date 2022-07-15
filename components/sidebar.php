fc7feb6e8ede00894a70350ff5546d5c91e4fdea
Renaldy <irenaldyleonarto@gmail.com>
2022-07-15 09:38:52 +0700

    fix: remove recent page

--- components/sidebar.php
+++ components/sidebar.php
@@ -52,15 +52,6 @@
                     </svg>
                     My Cloud
                 </a>
-                <a href="#"
-                   class="text-white hover:text-green-700 hover:bg-green-100 group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                    <svg class="mr-4 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                         stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                              d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                    </svg>
-                    Recent
-                </a>
             </div>
             <div class="mt-5 pt-5">
                 <div class="px-2 space-y-1">
@@ -100,15 +91,6 @@
                                   d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"/>
                         </svg>
                         My Cloud
-                    </a>
-                    <a href="#"
-                       class="text-white hover:text-green-700 hover:bg-green-100 group flex items-center px-2 py-2 text-sm leading-6 font-medium rounded-md">
-                        <svg class="mr-4 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
-                        </svg>
-                        Recent
                     </a>
                 </div>
                 <div class="mt-5 pt-5">
